ef51dfb695e404298081cc6b7912782c6b0da8a7
de1ux <evans.nathan.j@gmail.com>
2019-05-17 15:32:43 -0600

    Add initial stab at models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Guage(models.Model):
+    key = models.CharField(max_length=200)
+    value = models.CharField(max_length=600)
+
+class Layout(models.Model):
+    pass
+
+
+class LayoutItem(models.Model):
+    parent = models.ForeignKey(Layout, on_delete=models.CASCADE)
+    value = models.ForeignKey(Guage, on_delete=models.CASCADE)
--- settings.py
+++ settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'core.apps.CoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
