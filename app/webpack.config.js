ce832676b1ece46eca7ac5f1f13a076dff8db3e3
Nathan Evans <evans.nathan.j@gmail.com>
2019-05-19 00:47:52 -0600

    Add stubbing for client

---
+++ app/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+
+
+module.exports = function makeWebpackConfig() {
+  var config = {};
+
+  config.entry = {
+    index: "./index.tsx",
+  };
+  config.output = {
+    filename: '[name].bundle.js',
+    path: __dirname + "/dist"
+  };
+
+  // Enable sourcemaps for debugging webpack's output.
+  config.devtool = 'eval-source-map';
+
+  config.resolve = {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  };
+
+  config.module = {
+    rules: [
+      {test: /\.tsx?$/, loader: "awesome-typescript-loader"},
+      {test: /\.css$/, loader: "style-loader!css-loader"},
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+        loader: 'file-loader'
+      }
+    ]
+  };
+
+  config.plugins = [];
+
+  config.devServer = {
+    compress: true,
+    port: 3000,
+    hot: true
+  };
+
+  return config;
+}();
